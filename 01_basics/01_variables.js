39aedc4351ccda7c3bc43a259e2de8f9c593b42f
Sou000000111 <144088580+Sou000000111@users.noreply.github.com>
2026-01-16 18:50:12 +0000

    variables for javascript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const account_Id= 123344
+let accountEmail = "sinhasoumya@gmail.com";
+
+//var key word is not too much used in JS because of block scope and functional scope problem...
+var accountPassword = "2345";
+accountCity = "Midnapore"; // it will also automatically stores value in the memory without any datatype...
+
+let accountState; // if we does not assign any value then it shows undefined in the JS....
+
+
+console.log(account_Id);
+
+// table we used for tabular representation....
+console.table([account_Id, accountEmail, accountPassword, accountCity, accountState])
